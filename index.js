8893fc325adbc90fa64411a1998a9abc01d2b8be
null <adpatter@umich.edu>
2022-03-05 22:23:41 -0500

    et alia

---
+++ index.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let http = require('http');
+
+http.createServer(function (req, res) {
+
+  fs.readFile(__dirname + req.url, function (err,data) {
+
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+
+    res.writeHead(200);
+    data = eval(`\`${data}\``);
+
+    res.end(data);
+  });
+}).listen(8080);
